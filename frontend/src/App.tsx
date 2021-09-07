6d04a2ec78585f61188e362679bb2fc6e09ec050
= <mairarucaglia@gmail.com>
2021-09-07 10:51:55 -0300

    Gráficos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DonutChart from './components/DonutChart';
+import BarChart from './components/BarChart';
 import DataTable from './components/DataTable';
 import Footer from "./components/Footer";
 import NavBar from "./components/navbar";
@@ -5,13 +7,27 @@ import NavBar from "./components/navbar";
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
-      <h1 className="text-primary">Olá mundo</h1>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard Vendas</h1>
 
-      <DataTable />
-    </div>
-    <Footer />
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Total Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
