8d1362ec8cb2eb04b4244a176a2c38ac3ba3a77c
= <mairarucaglia@gmail.com>
2021-09-08 21:08:50 -0300

    Camadas

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDto;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDto> FindAll(){
+		List<Seller> result = repository.findAll();
+		
+		return result.stream().map(i -> new SellerDto(i)).collect(Collectors.toList());
+	}
+
+}
